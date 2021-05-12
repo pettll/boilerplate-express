32d3348efd9d6289fd92131614c477b71a2dce4c
Peterson Segatto Müller <p.smuller@outlook.com>
2021-05-12 12:41:34 +0000

    Add res.sendFile example

--- myApp.js
+++ myApp.js
@@ -3,7 +3,9 @@ var app = express();
 console.log("Hello World")
 
 app.get('/',function(req, res) {
-  res.send("Hello Express");
+  absolutePath = __dirname + /views/index.html
+
+  res.sendFile(path)
 })
 
 
